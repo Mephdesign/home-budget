4195a8bbf146225e1e2faac3ecb823417cbdceb3
karwackid <kontakt@damiankarwacki.pl>
2023-10-06 07:59:50 +0200

    podzial na stale/planowane

--- resources/views/livewire/planowane-list.blade.php
+++ resources/views/livewire/planowane-list.blade.php
@@ -2,7 +2,10 @@
     Wpływ: {{ $wplyw->kwota }} zł <br>
     Wydatki stałe: {{ $wydatki_stale_sum->kwota }} | Pozostało: {{ $wydatki_stale_sum->pozostalo }} <br>
     Wydatki planowane: {{ $wydatki_planowane_sum->kwota }}
-    Pozostało: {{ $wplyw->kwota - $wydatki_stale_sum->kwota - $wydatki_planowane_sum->kwota }} | Limit dzienny: 50 zł <br>
+    @php
+        $d = $wplyw->kwota - $wydatki_stale_sum->kwota - $wydatki_planowane_sum->kwota;
+    @endphp
+    Pozostało: {{ $wplyw->kwota - $wydatki_stale_sum->kwota - $wydatki_planowane_sum->kwota }} | Limit dzienny: {{ $d / 30 }} zł <br>
     @include('livewire.includes.planowane.create-stale-box')
     @include('livewire.includes.planowane.search-box')
     <div id="stale-list">
