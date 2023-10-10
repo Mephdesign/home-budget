0820f8efdef339813e7c1ce5081f939d98ff6c4a
karwackid <kontakt@damiankarwacki.pl>
2023-10-10 14:38:51 +0200

    Autoryzacja Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dzienne');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('dzienne');
+    });
+    Route::get('/plan-stale', function () {
+        return view('stale');
+    });
+    Route::get('/plan-planowane', function () {
+        return view('planowane');
+    });
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/plan-stale', function () {
-    return view('stale');
-});
-Route::get('/plan-planowane', function () {
-    return view('planowane');
-});
+
+require __DIR__.'/auth.php';
+
