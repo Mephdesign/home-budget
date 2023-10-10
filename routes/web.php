0a51a7f4c5bcac55b1516ffeb859db58e1ba72d1
karwackid <kontakt@damiankarwacki.pl>
2023-10-10 10:24:24 +0200

    poprawa wyswietlania wydatkow dziennych

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dzienne');
 });
 Route::get('/plan-stale', function () {
     return view('stale');
 });
 Route::get('/plan-planowane', function () {
     return view('planowane');
-});
-Route::get('/dzienne', function () {
-    return view('dzienne');
 });
